a304d89aa1c168d5ae0375c23990788b23e28f98
Dago Romer <dagoromer85@gmail.com>
2022-06-21 13:23:09 +0200

    change in first model

--- project_b/models/dago/dago01.sql
+++ project_b/models/dago/dago01.sql
@@ -1,12 +1,5 @@
 {{ config(materialized='table') }}
 
-with source_data as (
-
     select *
     from "BR_TEC_ARQ_PROYECTO0"."ARQUITECTURA"."mtcars_native"
-
-)
-
-select *
-from source_data
-where mpg > 15.0
+    where mpg > 15
