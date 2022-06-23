61c7d965ab052cedacd37e66571b7c32bcd40232
Dago Romer <dagoromer85@gmail.com>
2022-06-23 18:01:54 +0200

    revert after demo

--- project_b/models/dago/dagoagggears.sql
+++ project_b/models/dago/dagoagggears.sql
@@ -5,7 +5,7 @@ SELECT "gear",
     AVG("mpg") AS AVG_MPG,
     MIN("hp") AS MIN_HP,
     MAX("hp") AS MAX_HP,
-    AVG("hp") AS AVG_HP_DAGO
+    AVG("hp") AS AVG_HP
 FROM {{ ref('dago01') }}
 GROUP BY "gear"
-ORDER BY "gear" DESC
+ORDER BY "gear" ASC
