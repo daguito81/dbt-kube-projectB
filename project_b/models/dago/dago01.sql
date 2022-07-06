92e3c507272f40ad646f0b8dcee821ef78b1a6fa
Dago Romer <dagoromer85@gmail.com>
2022-07-06 14:05:36 +0200

    new fix

--- project_b/models/dago/dago01.sql
+++ project_b/models/dago/dago01.sql
@@ -1,12 +1,24 @@
 {{ config(
     materialized = 'mutua_incremental',
-    unique_key = 'row_names',
+    unique_key = 'ROW_NAMES',
     on_schema_change = 'fail',
     incremental_strategy = 'delete+insert',
     merge_update_columns = ['mpg', 'cyl']
 
 ) }}
 
-    SELECT *
+    SELECT 
+        "row_names" as ROW_NAMES,
+        "mpg" as MPG,
+        "cyl" as CYL,
+        "disp" as DISP,
+        "hp" as HP,
+        "drat" as DRAT,
+        "wt" as WT,
+        "qsec" as QSEC,
+        "vs" as VS,
+        "am" as AM,
+        "gear" as GEAR,
+        "carb" as CARB
     FROM "BR_TEC_ARQ_PROYECTO0"."ARQUITECTURA"."mtcars_native"
     WHERE "mpg" > 15
