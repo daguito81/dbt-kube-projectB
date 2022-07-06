85bdd957d9dfd11f25d50accedc83ef958b9eb1c
Dago Romer <dagoromer85@gmail.com>
2022-07-06 14:13:04 +0200

    more fixes

--- project_b/models/dago/dagoagggears.sql
+++ project_b/models/dago/dagoagggears.sql
@@ -1,11 +1,11 @@
 SELECT gear,
     COUNT(*) AS TOTAL,
-    MIN("mpg") AS MIN_MPG,
-    MAX("mpg") AS MAX_MPG,
-    AVG("mpg") AS AVG_MPG,
-    MIN("hp") AS MIN_HP,
-    MAX("hp") AS MAX_HP,
-    AVG("hp") AS AVG_HP_DAGO
+    MIN(mpg) AS MIN_MPG,
+    MAX(mpg) AS MAX_MPG,
+    AVG(mpg) AS AVG_MPG,
+    MIN(hp) AS MIN_HP,
+    MAX(hp) AS MAX_HP,
+    AVG(hp) AS AVG_HP_DAGO
 FROM {{ ref('dago01') }}
 GROUP BY gear
 ORDER BY gear DESC
