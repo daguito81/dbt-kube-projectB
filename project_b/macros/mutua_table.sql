75398d7df5496c511660e852c5a11a30f47ef110
Dago Romer <dagoromer85@gmail.com>
2022-07-06 15:44:58 +0200

    removed old_relation/backup

--- project_b/macros/mutua_table.sql
+++ project_b/macros/mutua_table.sql
@@ -8,21 +8,11 @@
                                                 schema=schema,
                                                 database=database,
                                                 type='table') -%}
-  
+
+    /*  
       See ../view/view.sql for more information about this relation.
-  */
-  {%- set backup_relation_type = 'table' if old_relation is none else old_relation.type -%}
-  {%- set backup_relation = api.Relation.create(identifier=backup_identifier,
-                                                schema=schema,
-                                                database=database,
-                                                type=backup_relation_type) -%}
-  -- as above, the backup_relation should not already exist
-  {%- set preexisting_backup_relation = adapter.get_relation(identifier=backup_identifier,
-                                                             schema=schema,
-                                                             database=database) -%}
+    */
 
-
-  {{ drop_relation_if_exists(preexisting_backup_relation) }}
 
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
@@ -40,9 +30,6 @@
   {%- endcall %}
 
   -- cleanup
-  {% if old_relation is not none %}
-      {{ adapter.rename_relation(old_relation, backup_relation) }}
-  {% endif %}
 
   {% do create_indexes(target_relation) %}
 
@@ -53,8 +40,6 @@
   -- `COMMIT` happens here
   {{ adapter.commit() }}
 
-  -- finally, drop the existing/backup relation after the commit
-  {{ drop_relation_if_exists(backup_relation) }}
 
   {{ run_hooks(post_hooks, inside_transaction=False) }}
 
